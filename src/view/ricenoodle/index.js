c470745ba043aa6b4f16c1599fcbe429f96e6d22
yayo <yangyong@navercorp.com>
2018-06-14 17:30:47 +0800

    improve background

--- src/view/ricenoodle/index.js
+++ src/view/ricenoodle/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Introduce from './introduce';
-import './style.scss';
 
 export default class Ricenoodle extends Component {
   render () {
--- src/view/ricenoodle/utilities/Background.js
+++ src/view/ricenoodle/utilities/Background.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import PageWrapper from '../../component/PageWrapper';
+import classnames from 'classnames';
+import style from '../style.scss';
 
 export default class Background extends Component {
   render () {
     return (
       <PageWrapper>
-        <div>
-          <h1>Background</h1>
+        <h3>说明</h3>
+        <p>快速设置背景色。预设 <code>12</code> 种颜色。</p>
+
+        <div className={classnames(style['util-bg'])}>
+          <div className="row mb-10">
+            <div className="col-6"><div className={classnames('bg-primary', style['bg-feature'])}></div></div>
+            <div className="col-6"><div className={classnames('bg-secondary', style['bg-feature'])}></div></div>
+          </div>
+
+          <div className="row mb-10">
+            <div className="col-6"><div className={classnames('bg-success', style['bg-feature'])}></div></div>
+            <div className="col-6"><div className={classnames('bg-info', style['bg-feature'])}></div></div>
+          </div>
+
+          <div className="row mb-10">
+            <div className="col-6"><div className={classnames('bg-warning', style['bg-feature'])}></div></div>
+            <div className="col-6"><div className={classnames('bg-danger', style['bg-feature'])}></div></div>
+          </div>
+
+          <div className="row mb-10">
+            <div className="col-6"><div className={classnames('bg-white border', style['bg-feature'])}></div></div>
+            <div className="col-6"><div className={classnames('bg-light', style['bg-feature'])}></div></div>
+          </div>
+
+          <div className="row mb-10">
+            <div className="col-6"><div className={classnames('bg-muted', style['bg-feature'])}></div></div>
+            <div className="col-6"><div className={classnames('bg-dark', style['bg-feature'])}></div></div>
+          </div>
+
+          <div className="row mb-10">
+            <div className="col-6"><div className={classnames('bg-black', style['bg-feature'])}></div></div>
+            <div className="col-6"><div className={classnames('bg-transparent border', style['bg-feature'])}></div></div>
+          </div>
         </div>
       </PageWrapper>
     );
