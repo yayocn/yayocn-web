828aadad0ee7a9f6e243d56573338627bbec6ed7
yayo <yangyong@navercorp.com>
2018-06-19 16:51:02 +0800

    apply highlight

--- src/view/component/Highlight/index.js
+++ src/view/component/Highlight/index.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import hljs from 'highlight/lib/vendor/highlight.js/highlight';
+import classnames from 'classnames';
+import { hljs } from 'highlight/lib/vendor/highlight.js/highlight';
+import xml from 'highlight/lib/vendor/highlight.js/languages/xml';
+import css from 'highlight/lib/vendor/highlight.js/languages/css';
+import js from 'highlight/lib/vendor/highlight.js/languages/javascript';
+import 'highlight/lib/vendor/highlight.js/styles/github.css';
+
+const exec = {
+  xml,
+  css,
+  js,
+}
 
 export default class Highlighter extends Component {
   componentDidMount() {
-    // hljs.highlightBlock(this.element)
+    const { lang = 'xml' } = this.props;
+    exec[lang](hljs);
+    hljs.highlightBlock(this.pre, ' ', true);
+  }
+
+  copyHandler = () => {
+
   }
 
   render () {
-    const { lang = 'xml' } = this.props;
-    console.log(hljs)
+    const { className } = this.props;
+
     return (
-      <div>
-        <pre ref={ref => { this.element = ref }}>
-          <code>
+      <div className="pt-relative border">
+        <pre className={classnames('p-20 m-0', className)} ref={ref => { this.pre = ref }}>
+          <code className="html border-0 bg-transparent">
             {this.props.children}
           </code>
         </pre>
+        <textarea className="hidden" defaultValue={this.props.children} ref={ref => { this.code = ref }}></textarea>
+        <button className="btn btn-sm btn-dark pt-absolute" style={{ top: '-1px', right: '-1px',}} onClick={this.copyHandler}>复制</button>
       </div>
     );
   }
--- src/view/ricenoodle/utilities/Background.js
+++ src/view/ricenoodle/utilities/Background.js
@@ -15,34 +15,62 @@ export default class Background extends Component {
           <div className="row mb-10">
             <div className="col-6">
               <div className={classnames('bg-primary', style['bg-feature'])}></div>
-              <Highlight />
+              <Highlight>
+                {
+                  `<div class="bg-primary">
+  ...
+</div>`
+                }
+              </Highlight>
             </div>
-            <div className="col-6"><div className={classnames('bg-secondary', style['bg-feature'])}></div></div>
+            <div className="col-6">
+              <div className={classnames('bg-secondary', style['bg-feature'])}></div>
+            </div>
           </div>
 
           <div className="row mb-10">
-            <div className="col-6"><div className={classnames('bg-success', style['bg-feature'])}></div></div>
-            <div className="col-6"><div className={classnames('bg-info', style['bg-feature'])}></div></div>
+            <div className="col-6">
+              <div className={classnames('bg-success', style['bg-feature'])}></div>
+            </div>
+            <div className="col-6">
+              <div className={classnames('bg-info', style['bg-feature'])}></div>
+            </div>
           </div>
 
           <div className="row mb-10">
-            <div className="col-6"><div className={classnames('bg-warning', style['bg-feature'])}></div></div>
-            <div className="col-6"><div className={classnames('bg-danger', style['bg-feature'])}></div></div>
+            <div className="col-6">
+              <div className={classnames('bg-warning', style['bg-feature'])}></div>
+            </div>
+            <div className="col-6">
+              <div className={classnames('bg-danger', style['bg-feature'])}></div>
+            </div>
           </div>
 
           <div className="row mb-10">
-            <div className="col-6"><div className={classnames('bg-white border', style['bg-feature'])}></div></div>
-            <div className="col-6"><div className={classnames('bg-light', style['bg-feature'])}></div></div>
+            <div className="col-6">
+              <div className={classnames('bg-white border', style['bg-feature'])}></div>
+            </div>
+            <div className="col-6">
+              <div className={classnames('bg-light', style['bg-feature'])}></div>
+            </div>
           </div>
 
           <div className="row mb-10">
-            <div className="col-6"><div className={classnames('bg-muted', style['bg-feature'])}></div></div>
-            <div className="col-6"><div className={classnames('bg-dark', style['bg-feature'])}></div></div>
+            <div className="col-6">
+              <div className={classnames('bg-muted', style['bg-feature'])}></div>
+            </div>
+            <div className="col-6">
+              <div className={classnames('bg-dark', style['bg-feature'])}></div>
+            </div>
           </div>
 
           <div className="row mb-10">
-            <div className="col-6"><div className={classnames('bg-black', style['bg-feature'])}></div></div>
-            <div className="col-6"><div className={classnames('bg-transparent border', style['bg-feature'])}></div></div>
+            <div className="col-6">
+              <div className={classnames('bg-black', style['bg-feature'])}></div>
+            </div>
+            <div className="col-6">
+              <div className={classnames('bg-transparent border', style['bg-feature'])}></div>
+            </div>
           </div>
         </div>
       </PageWrapper>
