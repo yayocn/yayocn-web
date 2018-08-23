7b62536d7f847085a195efb3d876dece71a01922
yayo <yangyong@navercorp.com>
2018-08-23 14:23:29 +0800

    improve dot

--- src/view/ricenoodle/components/Dot.js
+++ src/view/ricenoodle/components/Dot.js
@@ -6,19 +6,22 @@ import common from '../../../../public/scss/common.scss'
 import style from '../style.scss';
 
 const DOT = {
-  situation: [
-    { title: 'Primary', key: '', subTitle: '', className: 'dot dot-primary', },
-    { title: 'Secondary', key: '', subTitle: '', className: 'dot dot-secondary', },
-    { title: 'Success', key: '', subTitle: '', className: 'dot dot-success', },
-    { title: 'Info', key: '', subTitle: '', className: 'dot dot-info', },
-    { title: 'Warning', key: '', subTitle: '', className: 'dot dot-warning', },
-    { title: 'Danger', key: '', subTitle: '', className: 'dot dot-danger', },
-    { title: 'White', key: '', subTitle: '', className: 'dot dot-white', },
-    { title: 'Light', key: '', subTitle: '', className: 'dot dot-light', },
-    { title: 'Muted', key: '', subTitle: '', className: 'dot dot-muted', },
-    { title: 'Dark', key: '', subTitle: '', className: 'dot dot-dark', },
-    { title: 'Black', key: '', subTitle: '', className: 'dot dot-black', },
-    { title: 'Transparent', key: '', subTitle: '', className: 'dot dot-transparent', },
+  default: [
+    { title: 'Default', key: '', subTitle: '执行成功', className: 'dot dot-success', },
+    { title: 'Default', key: '', subTitle: '执行失败', className: 'dot dot-danger', },
+  ],
+  theme: [
+    { title: 'Primary', key: '', subTitle: '自定义含义', className: 'dot dot-primary', },
+    { title: 'Secondary', key: '', subTitle: '自定义含义', className: 'dot dot-secondary', },
+    { title: 'Success', key: '', subTitle: '自定义含义', className: 'dot dot-success', },
+    { title: 'Info', key: '', subTitle: '自定义含义', className: 'dot dot-info', },
+    { title: 'Warning', key: '', subTitle: '自定义含义', className: 'dot dot-warning', },
+    { title: 'Danger', key: '', subTitle: '自定义含义', className: 'dot dot-danger', },
+    { title: 'White', key: '', subTitle: '自定义含义', className: 'dot dot-white', },
+    { title: 'Light', key: '', subTitle: '自定义含义', className: 'dot dot-light', },
+    { title: 'Muted', key: '', subTitle: '自定义含义', className: 'dot dot-muted', },
+    { title: 'Dark', key: '', subTitle: '自定义含义', className: 'dot dot-dark', },
+    { title: 'Black', key: '', subTitle: '自定义含义', className: 'dot dot-black', },
   ],
 };
 
@@ -30,11 +33,11 @@ export default class Dot extends Component {
     const len = data.length;
     data.forEach((item, index) => {
       if (index !== len - 1) {
-        code += `This is a <span class="${item.className}"></span> dot.
+        code += `<p><span class="${item.className}"></span>：${item.subTitle}</p>
       
 `;
       } else {
-        code += `This is a <span class="${item.className}"></span> dot.`;
+        code += `<p><span class="${item.className}"></span>：${item.subTitle}</p>`;
       }
     })
 
@@ -44,13 +47,13 @@ export default class Dot extends Component {
   render () {
     return (
       <PageWrapper>
-        <h2>说明</h2>
-        <p>小圆点组件。一般可用于表示各种状态。</p>
+        <h1>圆点 <code>Dot</code></h1>
+        <p className="font-2x font-100">使用圆点组件，可以表示自定义状态。</p>
 
         <div className="divider divider-bold divider-lg"></div>
 
-        <h3>情景色</h3>
-        <p>预设 <code>12</code> 种情景色。</p>
+        <h2>实例</h2>
+        <p>在定义好的情况下，圆点组件可以用来代替文字说明。</p>
         <div className={classnames(style['util-badge'])}>
           <div className="row">
             <div className="col-12">
@@ -59,11 +62,11 @@ export default class Dot extends Component {
                   <div className="row">
                     <div className="col-12">
                       {
-                        DOT.situation.map((item, index) => {
+                        DOT.default.map((item, index) => {
                           return (
-                            <div className={classnames({ 'bg-dark text-white': item.title === 'White'})} key={index} >
-                              This is a <span className={classnames(item.className, style['badge-feature'])}></span> dot.
-                            </div>
+                            <p className="" key={index} >
+                              <span className={classnames(item.className, style['badge-feature'])}></span>：{item.subTitle}
+                            </p>
                           );
                         })
                       }
@@ -73,7 +76,42 @@ export default class Dot extends Component {
                 <div className={classnames(common['example-code'])}>
                   <Highlight>
                     {
-                      this.createCode('situation')
+                      this.createCode('default')
+                    }
+                  </Highlight>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="divider divider-lg"></div>
+
+        <h2>主题</h2>
+        <p>Ricenoodle 提供了<code>11</code> 中主题颜色。</p>
+        <div className={classnames(style['util-badge'])}>
+          <div className="row">
+            <div className="col-12">
+              <div className={classnames(common['feature-example'])}>
+                <div className={classnames(common['example-view'])}>
+                  <div className="row">
+                    <div className="col-12">
+                      {
+                        DOT.theme.map((item, index) => {
+                          return (
+                            <p className={classnames('p-5', { 'bg-dark text-white': item.title === 'White' })} key={index} >
+                              <span className={classnames(item.className, style['badge-feature'])}></span>：{item.subTitle}
+                            </p>
+                          );
+                        })
+                      }
+                    </div>
+                  </div>
+                </div>
+                <div className={classnames(common['example-code'])}>
+                  <Highlight>
+                    {
+                      this.createCode('theme')
                     }
                   </Highlight>
                 </div>
