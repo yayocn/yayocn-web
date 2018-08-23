8ff555d37836553ecff9344f9b8a3bc841d742a0
yayo <yangyong@navercorp.com>
2018-08-23 15:18:51 +0800

    improve elf

--- src/view/ricenoodle/components/Elf.js
+++ src/view/ricenoodle/components/Elf.js
@@ -6,7 +6,7 @@ import common from '../../../../public/scss/common.scss'
 import style from '../style.scss';
 
 const ELF = {
-  situation: [
+  theme: [
     { title: 'Primary', key: '', subTitle: '', className: 'elf elf-primary', },
     { title: 'Secondary', key: '', subTitle: '', className: 'elf elf-secondary', },
     { title: 'Success', key: '', subTitle: '', className: 'elf elf-success', },
@@ -27,6 +27,24 @@ const ELF = {
 
 export default class Breadcrumb extends Component {
 
+  createFormCode = () => {
+    return `<form class="form-hz">
+  <div class="form-group">
+    <label class="col-3 form-control-label"><span class="elf elf-primary">User Name</span></label>
+    <div class="col-9">
+      <input type="text" class="form-control" placeholder="enter user name"/>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="col-3 form-control-label"><span class="elf elf-primary">Password</span></label>
+    <div class="col-9">
+      <input type="password" class="form-control" placeholder="enter password"/>
+    </div>
+  </div>
+</form>`;
+  }
+  
   createCode = (target) => {
     const data = ELF[target];
     let code = ``;
@@ -47,13 +65,52 @@ export default class Breadcrumb extends Component {
   render () {
     return (
       <PageWrapper>
-        <h2>说明</h2>
-        <p>小精灵组件。用于对 <code>form</code> 表单的必填项，重点项等进行样式增强。</p>
+        <h1>精灵 <code>Elf</code></h1>
+        <p className="font-2x font-100">精灵组件，用于对 <code>form</code> 表单的必填项，重点项等进行增强。</p>
 
         <div className="divider divider-bold divider-lg"></div>
 
-        <h3>实例</h3>
-        <p></p>
+        <h2>实例</h2>
+        <div className={classnames(style['util-elf'])}>
+          <div className="row">
+            <div className="col-12">
+              <div className={classnames(common['feature-example'])}>
+                <div className={classnames(common['example-view'])}>
+                  <div className="row">
+                    <div className="col-lg-6 col-12">
+                      <form className="form-hz">
+                        <div className="form-group">
+                          <label className="col-3 form-control-label"><span className="elf elf-primary">User Name</span></label>
+                          <div className="col-9">
+                            <input type="text" className="form-control" placeholder="enter user name"/>
+                          </div>
+                        </div>
+
+                        <div className="form-group">
+                          <label className="col-3 form-control-label"><span className="elf elf-primary">Password</span></label>
+                          <div className="col-9">
+                            <input type="password" className="form-control" placeholder="enter password"/>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <div className={classnames(common['example-code'])}>
+                  <Highlight>
+                    {
+                      this.createFormCode()
+                    }
+                  </Highlight>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="divider divider-lg"></div>
+        <h2>主题</h2>
+        <p>Ricenoodle 提供了 <code>11</code> 种主题颜色。</p>
         <div className={classnames(style['util-elf'])}>
           <div className="row">
             <div className="col-12">
@@ -62,7 +119,7 @@ export default class Breadcrumb extends Component {
                   <div className="row">
                     <div className="col-12">
                       {
-                        ELF.situation.map((item, index) => {
+                        ELF.theme.map((item, index) => {
                           return (
                             <div className={classnames('ph-10 pv-5', {'bg-dark text-white': item.title === 'White'})} key={index}>
                               <span className={classnames(item.className, style['elf-feature'])} key={index} >
@@ -78,7 +135,7 @@ export default class Breadcrumb extends Component {
                 <div className={classnames(common['example-code'])}>
                   <Highlight>
                     {
-                      this.createCode('situation')
+                      this.createCode('theme')
                     }
                   </Highlight>
                 </div>
@@ -88,7 +145,7 @@ export default class Breadcrumb extends Component {
         </div>
 
         <div className="divider divider-lg"></div>
-        <h3>位置</h3>
+        <h2>位置</h2>
         <p>通过后缀 <code>-prev</code> 可以将小图标放在文字前面。</p>
         <div className={classnames(style['util-elf'])}>
           <div className="row">
@@ -100,8 +157,8 @@ export default class Breadcrumb extends Component {
                       {
                         ELF.position.map((item, index) => {
                           return (
-                            <div className={classnames('ph-10 pv-5', {'bg-dark text-white': item.title === 'White'})}>
-                              <span className={classnames(item.className, style['elf-feature'])} key={index} >
+                            <div className={classnames('ph-10 pv-5', {'bg-dark text-white': item.title === 'White'})} key={index} >
+                              <span className={classnames(item.className, style['elf-feature'])}>
                                 {item.title}
                               </span>
                             </div>
