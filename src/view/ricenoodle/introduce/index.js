63616b1e4f22d2b1b23feca51c506940ce1a0cce
yayo <yangyong@navercorp.com>
2018-08-31 17:55:04 +0800

    start info

--- src/view/ricenoodle/introduce/index.js
+++ src/view/ricenoodle/introduce/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import PageWrapper from '../../component/PageWrapper';
+import Highlight from '../../component/Highlight';
+import classnames from 'classnames';
+import common from '../../../../public/scss/common.scss';
 
-export default class Index extends Component {
+export default class Improve extends Component {
   render () {
     return (
       <PageWrapper>
-        <div>
-          <h1>简介</h1>
-        </div>
+        <h1>简介 <code>Introduce</code></h1>
+        <p className="font-2x font-100">YY UI，是 YaYo 出于兴趣和以学习的目的而形成的一个前端响应式框架。</p>
+
+        <div className="divider divider-lg divider-bold"></div>
+
+
       </PageWrapper>
     );
   }
--- src/view/ricenoodle/start/index.js
+++ src/view/ricenoodle/start/index.js
@@ -1,12 +1,71 @@
 import React, { Component } from 'react';
 import PageWrapper from '../../component/PageWrapper';
+import Highlight from '../../component/Highlight';
+import classnames from 'classnames';
+import common from '../../../../public/scss/common.scss';
 
-export default class Index extends Component {
+export default class Improve extends Component {
   render () {
     return (
       <PageWrapper>
+        <h1>开始 <code>Getting start</code></h1>
+        <p className="font-2x font-100">
+          开始使用 Ricenoodle。
+        </p>
+
+        <div className="divider divider-lg divider-bold"></div>
+
+        <h2>下载</h2>
+        <div className="row">
+          <div className="col-lg-3 col-md-2 col-1">
+            <h4>生产环境</h4>
+            <p>编译并且压缩后的css、javascript文件。</p>
+            <a href="files/yyui-1.1.2.zip" className="btn btn-secondary-outline">下载 Ricenoodle</a>
+          </div>
+          <div className="col-lg-3 col-md-2 col-1">
+            <h4>源码</h4>
+            <p>Ricenoodle 的资源文件。</p>
+            <a href="files/yyui-1.1.2.zip" className="btn btn-secondary-outline">Source</a>
+          </div>
+        </div>
+
+        <div className="divider divider-lg"></div>
+
+        <h2>使用</h2>
+        <p>Ricenoodle 有多种使用方式。</p>
+
+        <h3>使用本地文件</h3>
         <div>
-          <h1>开始</h1>
+          <div className="row">
+            <div className="col-12">
+              <div className={classnames(common['feature-example'])}>
+                <div className={classnames('mt-0', common['example-code'])}>
+                  <Highlight>
+                    &lt;link href="ricenoodle.min.css" rel="stylesheet"/&gt;<br/><br/>
+                    &lt;script src="jquery.min.js"&gt;&lt;/script&gt;<br/>
+                    &lt;script src="popper.min.js"&gt;&lt;/script&gt;<br/>
+                    &lt;script src="ricenoodle.min.js"&gt;&lt;/script&gt;
+                  </Highlight>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <h3 className="mt-20">NPM</h3>
+        <p>通过 <code>npm</code> 安装 <code>Ricenoodle</code>：</p>
+        <div>
+          <div className="row">
+            <div className="col-12">
+              <div className={classnames(common['feature-example'])}>
+                <div className={classnames('mt-0', common['example-code'])}>
+                  <Highlight>
+                    $ npm install ricenoodle
+                  </Highlight>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </PageWrapper>
     );
